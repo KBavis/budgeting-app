03537df47c9c1d55da19ee05d5763233130174f7
Kellen Bavis <kellenbavis10@gmail.com>
2024-02-13 14:28:21 -0500

    Altering application.yaml and build.gradle to remove Spring Security depedency for original testing purposes. Endpoints work, but Entities need to be altered to support CRUD operations appropiately

--- backend/src/main/java/com/bavis/budgetapp/dao/UserRepository.java
+++ backend/src/main/java/com/bavis/budgetapp/dao/UserRepository.java
@@ -1,6 +1,5 @@
 package com.bavis.budgetapp.dao;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,8 +7,4 @@ import org.springframework.stereotype.Repository;
 import com.bavis.budgetapp.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
-
-	public Optional<User> findByUserName(String username); 
-
-}
+public interface UserRepository extends JpaRepository<User, Long>{}
