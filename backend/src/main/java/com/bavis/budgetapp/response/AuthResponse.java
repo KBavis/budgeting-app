f4abb8b7560bb94573b76bd21009472e41d3a5df
Kellen Bavis <kellenbavis10@gmail.com>
2024-03-17 13:02:24 -0400

    Modification to JSON key name for AuthResponse for frontned distinction.

--- backend/src/main/java/com/bavis/budgetapp/response/AuthResponse.java
+++ backend/src/main/java/com/bavis/budgetapp/response/AuthResponse.java
@@ -1,6 +1,7 @@
 package com.bavis.budgetapp.response;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,5 +15,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Builder
 public class AuthResponse {
     private final String token; //jwt token
+    @JsonProperty("user")
     private final UserDetails userDetails; //details pertaining to authenticated user
 }
