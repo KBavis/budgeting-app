d930c1d090b308259f15cc56942d7f8f1d7e960b
Kellen Bavis <kellenbavis10@gmail.com>
2024-03-19 09:15:31 -0400

    Added CORS Configuration to enable Frontend calls to API.

--- backend/src/main/java/com/bavis/budgetapp/config/SecurityConfig.java
+++ backend/src/main/java/com/bavis/budgetapp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.bavis.budgetapp.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNullApi;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +14,8 @@ import org.springframework.security.config.annotation.web.configurers.SessionMan
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +28,8 @@ public class SecurityConfig {
     /**
      * Configure HTTP Security and Apply JWT Authentication Filter to HTTP Requests
      *
-     * @param http
-     *          - HTTP request security used to configure web security
-     * @return
-     *          - HTTP Security Filter Chain based on configurations
+     * @param http - HTTP request security used to configure web security
+     * @return - HTTP Security Filter Chain based on configurations
      * @throws Exception
      */
     @Bean
@@ -48,5 +49,22 @@ public class SecurityConfig {
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // Add JWT filter
 
         return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 }
