a459d0baec37b06709a0dd871817204785153877
Kellen Bavis <kellenbavis10@gmail.com>
2024-04-07 10:55:13 -0400

    Finalized implementation of Authentication Service tests

--- backend/src/test/java/com/bavis/budgetapp/services/AuthServiceTests.java
+++ backend/src/test/java/com/bavis/budgetapp/services/AuthServiceTests.java
@@ -1,6 +1,7 @@
 package com.bavis.budgetapp.services;
 
 import com.bavis.budgetapp.enumeration.Role;
+import com.bavis.budgetapp.exception.BadAuthenticationRequest;
 import com.bavis.budgetapp.exception.BadRegistrationRequestException;
 import com.bavis.budgetapp.exception.PlaidServiceException;
 import com.bavis.budgetapp.exception.UsernameTakenException;
@@ -11,7 +12,6 @@ import com.bavis.budgetapp.service.JwtService;
 import com.bavis.budgetapp.service.PlaidService;
 import com.bavis.budgetapp.service.UserService;
 import com.bavis.budgetapp.service.impl.AuthServiceImpl;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -20,10 +20,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.ArrayList;
 
@@ -55,18 +56,23 @@ public class AuthServiceTests {
     @InjectMocks
     private AuthServiceImpl authService;
 
-    private AuthRequest authRequest;
+    private AuthRequest registerAuthRequest;
+    private AuthRequest authenticateAuthRequest;
 
 
     @BeforeEach
     public void setup() {
         //init authService and auth request
         authService = new AuthServiceImpl(jwtService, userService, passwordEncoder, authenticationManager, plaidService);
-        authRequest = AuthRequest.builder()
+        registerAuthRequest = AuthRequest.builder()
                 .name("Test User")
                 .passwordOne("password")
                 .passwordTwo("password")
                 .username("test-user")
+                .build();
+        authenticateAuthRequest = AuthRequest.builder()
+                .username("username")
+                .passwordOne("password")
                 .build();
     }
 
@@ -84,8 +90,8 @@ public class AuthServiceTests {
 
         User testUser = User.builder()
                 .userId(userId)
-                .name(authRequest.getName())
-                .username(authRequest.getUsername())
+                .name(registerAuthRequest.getName())
+                .username(registerAuthRequest.getUsername())
                 .password(encryptedPassword)
                 .role(Role.USER)
                 .profileImage(null)
@@ -95,8 +101,8 @@ public class AuthServiceTests {
                 .build();
 
         //Mock
-        when(userService.existsByUsername(authRequest.getUsername())).thenReturn(false);
-        when(passwordEncoder.encode(authRequest.getPasswordOne())).thenReturn(encryptedPassword);
+        when(userService.existsByUsername(registerAuthRequest.getUsername())).thenReturn(false);
+        when(passwordEncoder.encode(registerAuthRequest.getPasswordOne())).thenReturn(encryptedPassword);
         when(userService.create(any(User.class))).thenAnswer(invocation -> {
             User user = invocation.getArgument(0); //update User Id accordingly when creating user
             user.setUserId(userId);
@@ -107,7 +113,7 @@ public class AuthServiceTests {
         when(jwtService.generateToken(any(User.class))).thenReturn(jwtToken);
 
         //Act
-        AuthResponse authResponse = authService.register(authRequest);
+        AuthResponse authResponse = authService.register(registerAuthRequest);
 
         //Assert
         assertNotNull(authResponse);
@@ -115,8 +121,8 @@ public class AuthServiceTests {
         assertEquals(authResponse.getUserDetails(), testUser);
 
         //Verify
-        verify(userService, times(1)).existsByUsername(authRequest.getUsername());
-        verify(passwordEncoder, times(1)).encode(authRequest.getPasswordOne());
+        verify(userService, times(1)).existsByUsername(registerAuthRequest.getUsername());
+        verify(passwordEncoder, times(1)).encode(registerAuthRequest.getPasswordOne());
         verify(userService, times(1)).create(any(User.class));
         verify(plaidService, times(1)).generateLinkToken(any(Long.class));
         verify(userService, times(1)).update(any(Long.class), any(User.class));
@@ -130,7 +136,7 @@ public class AuthServiceTests {
     @Test
     public void testRegister_UsernameTaken_Failure() {
         //Arrange
-        String username = authRequest.getUsername();
+        String username = registerAuthRequest.getUsername();
         String errorMessage = "The username [" + username + "] has already been taken.";
 
         //Mock
@@ -138,7 +144,7 @@ public class AuthServiceTests {
 
         //Act & Assert
         UsernameTakenException usernameTakenException = assertThrows(UsernameTakenException.class, () -> {
-            authService.register(authRequest);
+            authService.register(registerAuthRequest);
         });
         assertEquals(usernameTakenException.getMessage(), errorMessage);
 
@@ -241,8 +247,8 @@ public class AuthServiceTests {
         Long userId = 10L;
 
         //Mock
-        when(userService.existsByUsername(authRequest.getUsername())).thenReturn(false);
-        when(passwordEncoder.encode(authRequest.getPasswordOne())).thenReturn(encryptedPassword);
+        when(userService.existsByUsername(registerAuthRequest.getUsername())).thenReturn(false);
+        when(passwordEncoder.encode(registerAuthRequest.getPasswordOne())).thenReturn(encryptedPassword);
         when(userService.create(any(User.class))).thenAnswer(invocation -> {
             User user = invocation.getArgument(0); //update User Id accordingly when creating user
             user.setUserId(userId);
@@ -252,7 +258,7 @@ public class AuthServiceTests {
 
         //Act & Assert
         PlaidServiceException exception = assertThrows(PlaidServiceException.class, () -> {
-            authService.register(authRequest);
+            authService.register(registerAuthRequest);
         });
         assertEquals(exception.getMessage(), "Invalid Response Code When Generating Link Token Via PlaidClient: [404]");
     }
@@ -260,7 +266,6 @@ public class AuthServiceTests {
 
     /**
      * Validate necessary exception is being thrown when Jwt Service fails
-     * TODO: FIX THIS IMPLEMENTATION THAT IS NOT WOKRING
      */
     @Test
     public void testRegister_JwtServiceException_Failure() {
@@ -270,8 +275,8 @@ public class AuthServiceTests {
         Long userId = 10L;
         User testUser = User.builder()
                 .userId(userId)
-                .name(authRequest.getName())
-                .username(authRequest.getUsername())
+                .name(registerAuthRequest.getName())
+                .username(registerAuthRequest.getUsername())
                 .password(encryptedPassword)
                 .role(Role.USER)
                 .profileImage(null)
@@ -281,8 +286,8 @@ public class AuthServiceTests {
                 .build();
 
         // Mock
-        when(userService.existsByUsername(authRequest.getUsername())).thenReturn(false);
-        when(passwordEncoder.encode(authRequest.getPasswordOne())).thenReturn(encryptedPassword);
+        when(userService.existsByUsername(registerAuthRequest.getUsername())).thenReturn(false);
+        when(passwordEncoder.encode(registerAuthRequest.getPasswordOne())).thenReturn(encryptedPassword);
         when(userService.create(any(User.class))).thenAnswer(invocation -> {
             User user = invocation.getArgument(0);
             user.setUserId(userId);
@@ -296,10 +301,126 @@ public class AuthServiceTests {
 
         // Act & Assert
         RuntimeException exception = assertThrows(RuntimeException.class, () -> {
-            authService.register(authRequest);
+            authService.register(registerAuthRequest);
         });
         assertEquals(errorMessage, exception.getMessage());
 
+    }
+
+    /**
+     * Validate that authenticate can handle successful scenarios
+     */
+    @Test
+    public void testAuthenticate_Success() {
+        //Arrange
+        String username = authenticateAuthRequest.getUsername();
+        String password = authenticateAuthRequest.getPasswordOne();
+        String jwtToken = "jwt-token";
+        Long userId = 0L;
+        Authentication authenticatedUser = new UsernamePasswordAuthenticationToken(username, password);
+        User testUser = User.builder()
+                .userId(userId)
+                .name("Test User")
+                .username(username)
+                .password(password)
+                .role(Role.USER)
+                .profileImage(null)
+                .accounts(new ArrayList<>())
+                .categories(new ArrayList<>())
+                .linkToken(null)
+                .build();
+
+        //Mock
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(authenticatedUser);
+        when(userService.readByUsername(username)).thenReturn(testUser);
+        when(jwtService.generateToken(testUser)).thenReturn(jwtToken);
+
+        //Act
+        AuthResponse authResponse = authService.authenticate(authenticateAuthRequest);
+
+
+        //Assert
+        assertNotNull(authResponse);
+        assertEquals(authResponse.getToken(), jwtToken);
+        assertEquals(authResponse.getUserDetails(), testUser);
+
+        //Verify
+        verify(authenticationManager, times(1)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(userService, times(1)).readByUsername(username);
+        verify(jwtService, times(1)).generateToken(testUser);
+    }
+
+    /**
+     * Validate that authenticate correctly handles null username
+     */
+    @Test
+    public void testAuthenticate_NullUsername_Failed() {
+        //Arrange
+        AuthRequest invalidAuthRequest = AuthRequest.builder()
+                .username("")
+                .passwordOne("password")
+                .build();
+
+        //Act & Assert
+        BadAuthenticationRequest badAuthenticationRequest = assertThrows(BadAuthenticationRequest.class, () -> {
+            authService.authenticate(invalidAuthRequest);
+        });
+        assertEquals("Please fill out required fields: [Username, Password]", badAuthenticationRequest.getMessage());
+
+        //Verify
+        verify(authenticationManager, times(0)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(userService, times(0)).readByUsername("");
+        verify(jwtService, times(0)).generateToken(any(User.class));
+    }
+
+    /**
+     * Validate that authenticate correctly handles null password
+     */
+    @Test
+    public void testAuthenticate_NullPassword_Failed() {
+        //Arrange
+        AuthRequest invalidAuthRequest = AuthRequest.builder()
+                .username("username")
+                .passwordOne("")
+                .build();
+
+        //Act & Assert
+        BadAuthenticationRequest badAuthenticationRequest = assertThrows(BadAuthenticationRequest.class, () -> {
+            authService.authenticate(invalidAuthRequest);
+        });
+        assertEquals("Please fill out required fields: [Username, Password]", badAuthenticationRequest.getMessage());
+
+        //Verify
+        verify(authenticationManager, times(0)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(userService, times(0)).readByUsername("");
+        verify(jwtService, times(0)).generateToken(any(User.class));
+    }
+
+    /**
+     * Validate authenticate can correctly handle invalid credentials
+     */
+    @Test
+    public void testAuthenticate_InvalidCredentials_Failed() {
+        //Arrange
+        AuthRequest invalidAuthRequest = AuthRequest.builder()
+                .username("username")
+                .passwordOne("password")
+                .build();
+
+        //Mock
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenThrow(new BadCredentialsException("Invalid credentials"));
+
+
+        //Act & Assert
+        BadCredentialsException badCredentialsException = assertThrows(BadCredentialsException.class, () -> {
+            authService.authenticate(invalidAuthRequest);
+        });
+        assertEquals("Invalid credentials", badCredentialsException.getMessage());
+
+        //Verify
+        verify(authenticationManager, times(1)).authenticate(any(UsernamePasswordAuthenticationToken.class));
+        verify(userService, times(0)).readByUsername("username");
+        verify(jwtService, times(0)).generateToken(any(User.class));
     }
 
 }
