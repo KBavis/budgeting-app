268d64d88964545686f90bca29df7ec41dab8fb7
Kellen Bavis <kellenbavis10@gmail.com>
2024-04-28 01:44:43 -0400

    refactor: adding logging level for FeignClient and removing custom approach

--- backend/src/main/java/com/bavis/budgetapp/config/PlaidConfig.java
+++ backend/src/main/java/com/bavis/budgetapp/config/PlaidConfig.java
@@ -1,6 +1,7 @@
 package com.bavis.budgetapp.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Logger;
 import feign.codec.Decoder;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,8 +18,25 @@ public class PlaidConfig {
     @Value("${plaid.api.secret-key}")
     private String secretKey;
 
+    // Set the logging level
     @Bean
-    public Decoder accessTokenDecoder(ObjectMapper objectMapper) {
-        return new CustomAccessTokenDecoder(objectMapper);
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL; // Adjust this based on your requirement
     }
+
+    // Register the custom logger
+    @Bean
+    public Logger logger() {
+        return new OneLineLogger(); // Your custom logger
+    }
+
+    private static class OneLineLogger extends feign.Logger {
+        @Override
+        protected void log(String configKey, String format, Object... args) {
+            // Format log message as a single line
+            String message = String.format(configKey + " - " + format, args);
+            System.out.println(message);
+        }
+    }
+
 }
