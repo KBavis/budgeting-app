cc574a82fd3b029158fe90c4accba7c0f555267e
Kellen Bavis <kellenbavis10@gmail.com>
2024-05-04 13:11:32 -0400

    refactor: correcting URL for backend in utils

--- frontend/src/utils/url.js
+++ frontend/src/utils/url.js
@@ -1,5 +1,5 @@
-const apiUrl = "http://localhost:8080";
+// const apiUrl = "http://localhost:8080";
 
 //TODO: add local vs prod config to determine which URL to utilize
-// const apiUrl = "https://budget-app-backend-6cfcivaeda-pd.a.run.app";
+const apiUrl = "https://budget-app-backend-6cfcivaeda-pd.a.run.app";
 export default apiUrl;
