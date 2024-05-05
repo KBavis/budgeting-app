271251dc634fd79e4207d1263c1880dc86732747
Kellen Bavis <kellenbavis10@gmail.com>
2024-05-05 18:16:03 -0400

    refactor: adding javadocs implementation to CategoryService

--- backend/src/main/java/com/bavis/budgetapp/service/CategoryService.java
+++ backend/src/main/java/com/bavis/budgetapp/service/CategoryService.java
@@ -5,10 +5,63 @@ import com.bavis.budgetapp.entity.Category;
 
 import java.util.List;
 
+/**
+ * @author Kellen Bavis
+ *
+ * Service to house functionality regarding Category entities
+ */
 public interface CategoryService {
+	/**
+	 * Function to create multiple Category entities at once
+	 *
+	 * @param categories
+	 * 			- DTO to store information needed to create multiple Category entities
+	 * @return
+	 * 			- List of created Category entities
+	 */
 	List<Category> bulkCreate(BulkCategoryDto categories);
+
+	/**
+	 * Function to create a single Category entity
+	 *
+	 * @param category
+	 * 			- Category to be persisted within our database
+	 * @param categoryTypeId
+	 * 			- CategoryType entity ID to associate Category to
+	 * @return
+	 * 			- Created Category entity
+	 * @throws Exception
+	 * 			- Thrown in the case that an error occurs while creating a Category
+	 */
 	Category create(Category category, Long categoryTypeId) throws Exception;
+
+	/**
+	 * Function to update a Category
+	 *
+	 * @param category
+	 * 			- Category with updated properties
+	 * @param id
+	 * 			- ID of corresponding Category entity to be updated
+	 * @return
+	 * 			- Updated Category
+	 */
 	Category update(Category category, Long id);
+
+	/**
+	 * Function to fetch a specific Category
+	 *
+	 * @param categoryId
+	 * 			- Category ID corresponding to specific Category to be fetched
+	 * @return
+	 * 			- fetched Category
+	 */
 	Category read(Long categoryId);
+
+	/**
+	 * Function to delete a specific Category
+	 *
+	 * @param categoryId
+	 * 			- Category ID corresponding to specific Category to be deleted
+	 */
 	void delete(Long categoryId);
 }
