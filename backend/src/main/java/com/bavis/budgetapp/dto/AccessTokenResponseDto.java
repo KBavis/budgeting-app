2c0766929b0f6ba7ce73a0f08e687d865c2b49e7
Kellen Bavis <kellenbavis10@gmail.com>
2024-05-18 11:36:47 -0400

    feat: adding untracked file to VCS

---
+++ backend/src/main/java/com/bavis/budgetapp/dto/AccessTokenResponseDto.java
@@ -0,0 +1,27 @@
+package com.bavis.budgetapp.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Kellen Bavis
+ *
+ * DTO to encapsulate needed information returned by Plaid API when attempting to exchange public token for access token
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccessTokenResponseDto {
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("item_id")
+    private String itemId;
+
+    @JsonProperty("request_id")
+    private String requestId;
+}
