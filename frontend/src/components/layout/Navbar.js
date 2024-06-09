eaf1114a8719f11158e5e6f0c0bb6deff270dafa
Kellen Bavis <kellenbavis10@gmail.com>
2024-06-09 12:20:05 -0400

    feat: ensured Navbar doesn't appear for non-auth users

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,54 +1,55 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import AddTransaction from "../transaction/AddTransaction";
+import authContext from "../../context/auth/authContext";
 
+/**
+ *  NavBar component to introduce Multipage Navigation and logging out capabiltiies
+ */
 const Navbar = () => {
-   const [showModal, setShowModal] = useState(false);
-   // Function to toggle the modal
-   const toggleModal = () => {
-      setShowModal(!showModal);
-   };
+   const { user } = useContext(authContext);
 
    return (
-      <nav className="absolute top-0 right-0 w-full p-3">
-         <div className="container mx-auto">
-            <ul className="flex justify-end space-x-8">
-               <li>
-                  <Link
-                     to="/add-transaction"
-                     className="text-white text-base font-bold duration-500 hover:text-indigo-400"
-                  >
-                     Transactions
-                  </Link>
-               </li>
-               <li>
-                  <Link
-                     to="/add-account"
-                     className="text-white text-base font-bold duration-500 hover:text-indigo-400"
-                  >
-                     Accounts
-                  </Link>
-               </li>
-               <li>
-                  <Link
-                     to="/add-category"
-                     className="text-white text-base font-bold duration-500 hover:text-indigo-400"
-                  >
-                     Categories
-                  </Link>
-               </li>
-               <li>
-                  <Link
-                     to="/add-category"
-                     className="text-white text-base font-bold duration-500 hover:text-indigo-400"
-                  >
-                     Logout
-                  </Link>
-               </li>
-            </ul>
-         </div>
-         {showModal && <AddTransaction onClose={toggleModal} />}
-      </nav>
+      user && (
+         <nav className="absolute top-0 right-0 w-full p-3 z-[400]">
+            <div className="container mx-auto">
+               <ul className="flex justify-end space-x-8">
+                  <li>
+                     <Link
+                        to="/transactions"
+                        className="text-white text-base font-bold duration-500 hover:text-indigo-400"
+                     >
+                        Transactions
+                     </Link>
+                  </li>
+                  <li>
+                     <Link
+                        to="/accounts"
+                        className="text-white text-base font-bold duration-500 hover:text-indigo-400"
+                     >
+                        Accounts
+                     </Link>
+                  </li>
+                  <li>
+                     <Link
+                        to="/categories"
+                        className="text-white text-base font-bold duration-500 hover:text-indigo-400"
+                     >
+                        Categories
+                     </Link>
+                  </li>
+                  <li>
+                     <Link
+                        to="/logout"
+                        className="text-white text-base font-bold duration-500 hover:text-indigo-400"
+                     >
+                        Logout
+                     </Link>
+                  </li>
+               </ul>
+            </div>
+         </nav>
+      )
    );
 };
 
