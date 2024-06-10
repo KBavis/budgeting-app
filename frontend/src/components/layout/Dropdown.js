139b6619c5d0802c6441b5bc57855407c5cb49f6
Kellen Bavis <kellenbavis10@gmail.com>
2024-06-09 23:51:04 -0400

    refactor: updated Dropdown button color

--- frontend/src/components/layout/Dropdown.js
+++ frontend/src/components/layout/Dropdown.js
@@ -4,40 +4,41 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { FiDollarSign, FiList, FiUser, FiPlus } from "react-icons/fi";
 
 const DropdownMenu = ({
-   dropdownVisible,
-   setDropdownVisible,
-   handleShowAddTransactionModal,
+  dropdownVisible,
+  setDropdownVisible,
+  handleShowAddTransactionModal,
 }) => {
-   return (
-      <DropdownButton
-         className="absolute top-0 left-0 mt-4 ml-4 z-[500] "
-         title={<FiPlus size={30} />}
-         show={dropdownVisible}
-         onToggle={() => setDropdownVisible(!dropdownVisible)}
+  return (
+    <DropdownButton
+      className="absolute top-0 left-0 mt-4 ml-4 z-[105] "
+      title={<FiPlus size={30} />}
+      show={dropdownVisible}
+      variant="secondary"
+      onToggle={() => setDropdownVisible(!dropdownVisible)}
+    >
+      <Dropdown.Item
+        className="dropdown-item flex items-center mb-2 font-bold"
+        onClick={handleShowAddTransactionModal}
       >
-         <Dropdown.Item
-            className="dropdown-item flex items-center mb-2 font-bold"
-            onClick={handleShowAddTransactionModal}
-         >
-            <FiDollarSign className="mr-2" />
-            Add Transaction
-         </Dropdown.Item>
-         <Dropdown.Item
-            className="dropdown-item flex items-center mb-2 font-bold"
-            onClick={() => console.log("Add Category clicked")}
-         >
-            <FiList className="mr-2" />
-            Add Category
-         </Dropdown.Item>
-         <Dropdown.Item
-            className="dropdown-item flex items-center font-bold"
-            onClick={() => console.log("Add Account clicked")}
-         >
-            <FiUser className="mr-2" />
-            Add Account
-         </Dropdown.Item>
-      </DropdownButton>
-   );
+        <FiDollarSign className="mr-2" />
+        Add Transaction
+      </Dropdown.Item>
+      <Dropdown.Item
+        className="dropdown-item flex items-center mb-2 font-bold"
+        onClick={() => console.log("Add Category clicked")}
+      >
+        <FiList className="mr-2" />
+        Add Category
+      </Dropdown.Item>
+      <Dropdown.Item
+        className="dropdown-item flex items-center font-bold"
+        onClick={() => console.log("Add Account clicked")}
+      >
+        <FiUser className="mr-2" />
+        Add Account
+      </Dropdown.Item>
+    </DropdownButton>
+  );
 };
 
 export default DropdownMenu;
