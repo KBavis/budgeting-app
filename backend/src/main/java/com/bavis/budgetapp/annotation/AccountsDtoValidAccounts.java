08cf8aaf2427b9ce17caed2f92dec87873dcf922
Kellen Bavis <kellenbavis10@gmail.com>
2024-06-23 14:09:08 -0400

    refactor: adding clarity in AccountsDto annotation

--- backend/src/main/java/com/bavis/budgetapp/annotation/AccountsDtoValidAccounts.java
+++ backend/src/main/java/com/bavis/budgetapp/annotation/AccountsDtoValidAccounts.java
@@ -9,11 +9,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @author Kellen Bavis
+ *
+ * Annotations for ensuring Account IDs in AccountsDto are valid
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Constraint(validatedBy = AccountsDtoAccountsValidator.class)
 public @interface AccountsDtoValidAccounts {
-    String message() default "The provided list of Account ID's to sync transactions for contains at least one invalid entry";
+    String message() default "The provided list of Account ID's contains at least one invalid entry";
 
     Class<?>[] groups() default {};
 
