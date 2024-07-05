18b98cdad91e05b0a675119dd8abbbea295c1240
Kellen Bavis <kellenbavis10@gmail.com>
2024-07-05 19:06:00 -0400

    feat: added endpoint for refreshing link token

--- frontend/src/context/auth/AuthState.js
+++ frontend/src/context/auth/AuthState.js
@@ -3,17 +3,19 @@ import authReducer from "./authReducer";
 import axios from "axios";
 import apiUrl from "../../utils/url";
 import {
-   REGISTER_FAIL,
-   AUTH_FAIL,
-   LOGOUT,
-   AUTH_SUCCESS,
-   REGISTER_SUCCESS,
-   CLEAR_ERRORS,
-   SET_LOADING,
-   FETCH_AUTH_USER_FAIL,
-   FETCH_AUTH_USER_SUCCESS,
-   LOGOUT_SUCCESS,
-   LOGOUT_FAIL,
+  REGISTER_FAIL,
+  AUTH_FAIL,
+  LOGOUT,
+  AUTH_SUCCESS,
+  REGISTER_SUCCESS,
+  CLEAR_ERRORS,
+  SET_LOADING,
+  FETCH_AUTH_USER_FAIL,
+  FETCH_AUTH_USER_SUCCESS,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
+  REFRESH_LINK_TOKEN_SUCCESS,
+  REFRESH_LINK_TOKEN_FAILURE,
 } from "./types";
 import initalState from "./initalState";
 import AuthContext from "./authContext";
@@ -27,121 +29,140 @@ import setAuthToken from "../../utils/setAuthToken";
  * @returns
  */
 const AuthState = (props) => {
-   const [state, dispatch] = useReducer(authReducer, initalState);
+  const [state, dispatch] = useReducer(authReducer, initalState);
 
-   /**
-    * Functionality to Register a new User
-    *
-    * @param formData
-    *       - payload to send to our REST API
-    */
-   const register = async (formData) => {
-      const config = {
-         headers: {
-            "Content-Type": "application/json",
-         },
-      };
-      try {
-         const res = await axios.post(
-            `${apiUrl}/auth/register`,
-            formData,
-            config
-         );
-         dispatch({ type: REGISTER_SUCCESS, payload: res.data });
-      } catch (err) {
-         console.error(err);
-         dispatch({ type: REGISTER_FAIL, payload: err.response.data.error });
-      }
-   };
+  /**
+   * Functionality to Register a new User
+   *
+   * @param formData
+   *       - payload to send to our REST API
+   */
+  const register = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post(`${apiUrl}/auth/register`, formData, config);
+      dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    } catch (err) {
+      console.error(err);
+      dispatch({ type: REGISTER_FAIL, payload: err.response.data.error });
+    }
+  };
 
-   /**
-    * Functionality to Authenticate User
-    *
-    * @param formData
-    *       - payload to send to our REST API
-    */
-   const login = async (formData) => {
-      const config = {
-         headers: {
-            "Content-Type": "application/json",
-         },
-      };
+  /**
+   * Functionality to Authenticate User
+   *
+   * @param formData
+   *       - payload to send to our REST API
+   */
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-      try {
-         const res = await axios.post(
-            `${apiUrl}/auth/authenticate`,
-            formData,
-            config
-         );
-         dispatch({ type: AUTH_SUCCESS, payload: res.data });
-      } catch (err) {
-         console.error(err);
-         dispatch({ type: AUTH_FAIL, payload: err.response.data.error });
-      }
-   };
+    try {
+      const res = await axios.post(
+        `${apiUrl}/auth/authenticate`,
+        formData,
+        config
+      );
+      dispatch({ type: AUTH_SUCCESS, payload: res.data });
+    } catch (err) {
+      console.error(err);
+      dispatch({ type: AUTH_FAIL, payload: err.response.data.error });
+    }
+  };
 
-   /**
-    * Function to fetch the currently authenticated user
-    */
-   const fetchAuthenticatedUser = async () => {
-      if (localStorage.token) {
-         setAuthToken(localStorage.token);
-      }
+  /**
+   * Functionality to refresh a User's Plaid Link Token
+   */
+  const refreshLinkToken = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    try {
+      const res = await axios.put(`${apiUrl}/user/refresh/link-token`);
+      dispatch({ type: REFRESH_LINK_TOKEN_SUCCESS, payload: res.data });
+    } catch (err) {
+      console.error(err);
+      dispatch({
+        type: REFRESH_LINK_TOKEN_FAILURE,
+        payload: err.response.data.error,
+      });
+    }
+  };
 
-      try {
-         const res = await axios.get(`${apiUrl}/auth`);
-         dispatch({ type: FETCH_AUTH_USER_SUCCESS, payload: res.data });
-      } catch (err) {
-         console.error(err);
-         dispatch({
-            type: FETCH_AUTH_USER_FAIL,
-            payload: err.response.data.error,
-         });
-      }
-   };
+  /**
+   * Function to fetch the currently authenticated user
+   */
+  const fetchAuthenticatedUser = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
 
-   const logout = async () => {
-      if (localStorage.token) {
-         setAuthToken(localStorage.token);
-      }
+    try {
+      const res = await axios.get(`${apiUrl}/auth`);
+      dispatch({ type: FETCH_AUTH_USER_SUCCESS, payload: res.data });
+    } catch (err) {
+      console.error(err);
+      dispatch({
+        type: FETCH_AUTH_USER_FAIL,
+        payload: err.response.data.error,
+      });
+    }
+  };
 
-      try {
-         await axios.delete(`${apiUrl}/auth/logout`);
-         dispatch({ type: LOGOUT_SUCCESS, payload: null });
-      } catch (err) {
-         console.error(err);
-      }
-   };
+  /**
+   * Functionality to log a user out of Budget Application
+   */
+  const logout = async () => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
 
-   /**
-    * Functionality to Clear Errors
-    */
-   const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+    try {
+      await axios.delete(`${apiUrl}/auth/logout`);
+      dispatch({ type: LOGOUT_SUCCESS, payload: null });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-   /**
-    *  Functionality to Set Loading
-    */
-   const setLoading = () => dispatch({ type: SET_LOADING });
+  /**
+   * Functionality to Clear Errors
+   */
+  const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
-   return (
-      <AuthContext.Provider
-         value={{
-            token: state.token,
-            isAuthenticated: state.isAuthenticated,
-            loading: state.loading,
-            user: state.user,
-            error: state.error,
-            register,
-            login,
-            clearErrors,
-            setLoading,
-            fetchAuthenticatedUser,
-            logout,
-         }}
-      >
-         {props.children}
-      </AuthContext.Provider>
-   );
+  /**
+   *  Functionality to Set Loading
+   */
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+        register,
+        login,
+        clearErrors,
+        setLoading,
+        fetchAuthenticatedUser,
+        logout,
+        refreshLinkToken,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthState;
--- frontend/src/context/auth/authReducer.js
+++ frontend/src/context/auth/authReducer.js
@@ -1,69 +1,83 @@
 import {
-   AUTH_FAIL,
-   AUTH_SUCCESS,
-   REGISTER_FAIL,
-   REGISTER_SUCCESS,
-   CLEAR_ERRORS,
-   SET_LOADING,
-   FETCH_AUTH_USER_FAIL,
-   FETCH_AUTH_USER_SUCCESS,
-   LOGOUT_SUCCESS,
+  AUTH_FAIL,
+  AUTH_SUCCESS,
+  REGISTER_FAIL,
+  REGISTER_SUCCESS,
+  CLEAR_ERRORS,
+  SET_LOADING,
+  FETCH_AUTH_USER_FAIL,
+  FETCH_AUTH_USER_SUCCESS,
+  LOGOUT_SUCCESS,
+  REFRESH_LINK_TOKEN_FAILURE,
+  REFRESH_LINK_TOKEN_SUCCESS,
 } from "./types";
 
 /**
  * Reducer for handling updating state for specific actions for Authentication
  */
 export default (state, action) => {
-   switch (action.type) {
-      case AUTH_SUCCESS:
-      case REGISTER_SUCCESS:
-         localStorage.setItem("token", action.payload.token); //put JWT Token in Storage
-         return {
-            ...state,
-            ...action.payload,
-            isAuthenticated: true,
-            loading: false,
-         };
-      case REGISTER_FAIL:
-      case AUTH_FAIL:
-      case FETCH_AUTH_USER_FAIL:
-         localStorage.removeItem("token");
-         return {
-            ...state,
-            token: null,
-            isAuthenticated: false,
-            loading: false,
-            user: null,
-            error: action.payload,
-         };
-      case FETCH_AUTH_USER_SUCCESS:
-         return {
-            ...state,
-            user: action.payload,
-            isAuthenticated: true,
-            loading: false,
-            token: localStorage.token,
-         };
-      case LOGOUT_SUCCESS:
-         localStorage.removeItem("token");
-         return {
-            ...state,
-            user: null,
-            isAuthenticated: false,
-            loading: false,
-            token: null,
-         };
-      case CLEAR_ERRORS:
-         return {
-            ...state,
-            error: null,
-         };
-      case SET_LOADING:
-         return {
-            ...state,
-            loading: true,
-         };
-      default:
-         return state;
-   }
+  switch (action.type) {
+    case AUTH_SUCCESS:
+    case REGISTER_SUCCESS:
+      localStorage.setItem("token", action.payload.token); //put JWT Token in Storage
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+    case AUTH_FAIL:
+    case FETCH_AUTH_USER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
+    case FETCH_AUTH_USER_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+        loading: false,
+        token: localStorage.token,
+      };
+    case LOGOUT_SUCCESS:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+        loading: false,
+        token: null,
+      };
+    case REFRESH_LINK_TOKEN_SUCCESS:
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          linkToken: action.payload,
+        },
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    case REFRESH_LINK_TOKEN_FAILURE: //do nothing if failure
+      return {
+        ...state,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
 };
