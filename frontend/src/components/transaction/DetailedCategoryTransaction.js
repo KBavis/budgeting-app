c9026fa85f4a190c6447e31e65a55a37d9f79141
Kellen Bavis <kellenbavis10@gmail.com>
2024-07-07 14:25:50 -0400

    refactor: added larger spacing b/t DetailedCategory components on CategoryTypePage and updated Dropdown to be fixed

--- frontend/src/components/transaction/DetailedCategoryTransaction.js
+++ frontend/src/components/transaction/DetailedCategoryTransaction.js
@@ -19,122 +19,124 @@ import transactionContext from "../../context/transaction/transactionContext";
  *       - Functionality to display AssignCategory modal
  */
 const DetailedCategoryTransaction = ({
-  transaction,
-  handleShowSplitTransactionModal,
-  handleShowReduceTransactionModal,
-  handleShowRenameTransactionModal,
-  handleShowAssignCategoryModal,
+   transaction,
+   handleShowSplitTransactionModal,
+   handleShowReduceTransactionModal,
+   handleShowRenameTransactionModal,
+   handleShowAssignCategoryModal,
 }) => {
-  // Determine rounded amount for Transaction
-  const roundedAmount = Math.round(transaction.amount);
+   // Determine rounded amount for Transaction
+   const roundedAmount = Math.round(transaction.amount);
 
-  //Local State
-  const [dropdownVisible, setDropdownVisible] = useState(false);
+   //Local State
+   const [dropdownVisible, setDropdownVisible] = useState(false);
 
-  //Glboal State
-  const { deleteTransaction } = useContext(transactionContext);
+   //Glboal State
+   const { deleteTransaction } = useContext(transactionContext);
 
-  const handleDropdownToggle = (isOpen) => {
-    setDropdownVisible(isOpen);
-  };
+   const handleDropdownToggle = (isOpen) => {
+      setDropdownVisible(isOpen);
+   };
 
-  //Handle removing a Transaction
-  const handleDeleteTransaction = () => {
-    deleteTransaction(transaction.transactionId);
-    setDropdownVisible(false);
-  };
+   //Handle removing a Transaction
+   const handleDeleteTransaction = () => {
+      deleteTransaction(transaction.transactionId);
+      setDropdownVisible(false);
+   };
 
-  //Handle splitting a Transaction
-  const handleSplitTransaction = () => {
-    handleShowSplitTransactionModal(transaction);
-    setDropdownVisible(false);
-  };
+   //Handle splitting a Transaction
+   const handleSplitTransaction = () => {
+      handleShowSplitTransactionModal(transaction);
+      setDropdownVisible(false);
+   };
 
-  //Handle re-assinging Transaction
-  const handleReassignTransaction = () => {
-    handleShowAssignCategoryModal(transaction);
-    setDropdownVisible(false);
-  };
+   //Handle re-assinging Transaction
+   const handleReassignTransaction = () => {
+      handleShowAssignCategoryModal(transaction);
+      setDropdownVisible(false);
+   };
 
-  //Handle re-naming Transaction
-  const handleRenameTransaction = () => {
-    handleShowRenameTransactionModal(transaction);
-    setDropdownVisible(false);
-  };
+   //Handle re-naming Transaction
+   const handleRenameTransaction = () => {
+      handleShowRenameTransactionModal(transaction);
+      setDropdownVisible(false);
+   };
 
-  //Handle reducing Transaction
-  const handleReduceTransaction = () => {
-    handleShowReduceTransactionModal(transaction);
-    setDropdownVisible(false);
-  };
+   //Handle reducing Transaction
+   const handleReduceTransaction = () => {
+      handleShowReduceTransactionModal(transaction);
+      setDropdownVisible(false);
+   };
 
-  return (
-    <div className="bg-indigo-900 rounded-lg p-3 shadow-md flex items-center justify-between mb-3">
-      <div className="flex items-center space-x-2 flex-grow overflow-hidden">
-        {transaction.logoUrl ? (
-          <img
-            src={transaction.logoUrl}
-            alt="Transaction Logo"
-            className="w-8 h-8 rounded-full flex-shrink-0"
-          />
-        ) : (
-          <img
-            src="https://bavis-budget-app-bucket.s3.amazonaws.com/default-avatar-icon-of-social-media-user-vector.jpg"
-            className="w-8 h-8 rounded-full flex-shrink-0"
-            alt="Default Avatar"
-          />
-        )}
-        <div className="text-white text-left flex-grow">
-          <p className="text-sm font-bold break-words">{transaction.name}</p>
-          <p className="text-sm">${roundedAmount}</p>
-        </div>
+   return (
+      <div className="bg-indigo-900 rounded-lg p-3 shadow-md flex items-center justify-between mb-3">
+         <div className="flex items-center space-x-2 flex-grow overflow-hidden">
+            {transaction.logoUrl ? (
+               <img
+                  src={transaction.logoUrl}
+                  alt="Transaction Logo"
+                  className="w-8 h-8 rounded-full flex-shrink-0"
+               />
+            ) : (
+               <img
+                  src="https://bavis-budget-app-bucket.s3.amazonaws.com/default-avatar-icon-of-social-media-user-vector.jpg"
+                  className="w-8 h-8 rounded-full flex-shrink-0"
+                  alt="Default Avatar"
+               />
+            )}
+            <div className="text-white text-left flex-grow">
+               <p className="text-sm font-bold break-words">
+                  {transaction.name}
+               </p>
+               <p className="text-sm">${roundedAmount}</p>
+            </div>
+         </div>
+         <Dropdown show={dropdownVisible} onToggle={handleDropdownToggle}>
+            <Dropdown.Toggle
+               as="button"
+               className="text-white bg-transparent border-none p-0 m-0"
+            >
+               <FaEllipsisV size={20} />
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu
+               align="end"
+               className="bg-white text-black text-sm text-center custom-dropdown-menu"
+            >
+               <Dropdown.Item
+                  className="dropdown-item font-semibold"
+                  onClick={handleDeleteTransaction}
+               >
+                  Delete
+               </Dropdown.Item>
+               <Dropdown.Item
+                  className="dropdown-item font-semibold"
+                  onClick={handleSplitTransaction}
+               >
+                  Split
+               </Dropdown.Item>
+               <Dropdown.Item
+                  className="dropdown-item font-semibold"
+                  onClick={handleReassignTransaction}
+               >
+                  Assign Category
+               </Dropdown.Item>
+               <Dropdown.Item
+                  className="dropdown-item font-semibold"
+                  onClick={handleRenameTransaction}
+               >
+                  Rename
+               </Dropdown.Item>
+               <Dropdown.Item
+                  className="dropdown-item font-semibold"
+                  onClick={handleReduceTransaction}
+               >
+                  Reduce
+               </Dropdown.Item>
+            </Dropdown.Menu>
+         </Dropdown>
       </div>
-      <Dropdown show={dropdownVisible} onToggle={handleDropdownToggle}>
-        <Dropdown.Toggle
-          as="button"
-          className="text-white bg-transparent border-none p-0 m-0"
-        >
-          <FaEllipsisV size={20} />
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu
-          align="end"
-          className="bg-white text-black text-sm text-center"
-        >
-          <Dropdown.Item
-            className="dropdown-item font-semibold"
-            onClick={handleDeleteTransaction}
-          >
-            Delete
-          </Dropdown.Item>
-          <Dropdown.Item
-            className="dropdown-item font-semibold"
-            onClick={handleSplitTransaction}
-          >
-            Split
-          </Dropdown.Item>
-          <Dropdown.Item
-            className="dropdown-item font-semibold"
-            onClick={handleReassignTransaction}
-          >
-            Re-Assign Category
-          </Dropdown.Item>
-          <Dropdown.Item
-            className="dropdown-item font-semibold"
-            onClick={handleRenameTransaction}
-          >
-            Rename
-          </Dropdown.Item>
-          <Dropdown.Item
-            className="dropdown-item font-semibold"
-            onClick={handleReduceTransaction}
-          >
-            Reduce
-          </Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
-  );
+   );
 };
 
 export default DetailedCategoryTransaction;
--- frontend/src/pages/CategoryTypePage.js
+++ frontend/src/pages/CategoryTypePage.js
@@ -19,186 +19,192 @@ import AssignCategoryModal from "../components/transaction/AssignCategoryModal";
  *          - CategoryType to generate page for
  */
 const CategoryTypePage = ({ categoryType }) => {
-  const { categoryTypes, fetchCategoryTypes } = useContext(categoryTypeContext);
-  const { categories, fetchCategories } = useContext(categoryContext);
-  const { user, fetchAuthenticatedUser } = useContext(authContext);
-  const { transactions, fetchTransactions } = useContext(transactionContext);
-  const [filteredCategories, setFilteredCategories] = useState([]);
-  const [showSplitTransactionModal, setShowSplitTransactionModal] =
-    useState(false);
-  const [showReduceTransactionModal, setShowReduceTransactionModal] =
-    useState(false);
-  const [showRenameTransactionModal, setShowRenameTransactionModal] =
-    useState(false);
-  const [showAssignCategoryModal, setShowAssignCategoryModal] = useState(false);
-  const [currentTransaction, setCurrentTransaction] = useState(null);
-  const navigate = useNavigate();
+   const { categoryTypes, fetchCategoryTypes } =
+      useContext(categoryTypeContext);
+   const { categories, fetchCategories } = useContext(categoryContext);
+   const { user, fetchAuthenticatedUser } = useContext(authContext);
+   const { transactions, fetchTransactions } = useContext(transactionContext);
+   const [filteredCategories, setFilteredCategories] = useState([]);
+   const [showSplitTransactionModal, setShowSplitTransactionModal] =
+      useState(false);
+   const [showReduceTransactionModal, setShowReduceTransactionModal] =
+      useState(false);
+   const [showRenameTransactionModal, setShowRenameTransactionModal] =
+      useState(false);
+   const [showAssignCategoryModal, setShowAssignCategoryModal] =
+      useState(false);
+   const [currentTransaction, setCurrentTransaction] = useState(null);
+   const navigate = useNavigate();
 
-  const initialFetchRef = useRef(false);
+   const initialFetchRef = useRef(false);
 
-  //Filter The Categories for Current Category Type
-  useEffect(() => {
-    const currentCategoryType = categoryTypes.find(
-      (ct) => ct.name.toLowerCase() === categoryType.toLowerCase()
-    );
-
-    if (currentCategoryType) {
-      const filtered = categories.filter(
-        (category) =>
-          category.categoryType.categoryTypeId ===
-          currentCategoryType.categoryTypeId
+   //Filter The Categories for Current Category Type
+   useEffect(() => {
+      const currentCategoryType = categoryTypes.find(
+         (ct) => ct.name.toLowerCase() === categoryType.toLowerCase()
       );
-      setFilteredCategories(filtered);
-    }
-  }, [categoryTypes, categories, categoryType]);
 
-  //Fetch All Values On Refresh
-  useEffect(() => {
-    const fetch = async () => {
-      if (!user && localStorage.token) {
-        await fetchAuthenticatedUser();
+      if (currentCategoryType) {
+         const filtered = categories.filter(
+            (category) =>
+               category.categoryType.categoryTypeId ===
+               currentCategoryType.categoryTypeId
+         );
+         setFilteredCategories(filtered);
       }
+   }, [categoryTypes, categories, categoryType]);
 
-      if (!categoryTypes || categoryTypes.length === 0) {
-        await fetchCategoryTypes();
+   //Fetch All Values On Refresh
+   useEffect(() => {
+      const fetch = async () => {
+         if (!user && localStorage.token) {
+            await fetchAuthenticatedUser();
+         }
+
+         if (!categoryTypes || categoryTypes.length === 0) {
+            await fetchCategoryTypes();
+         }
+
+         if (!categories || categories.length === 0) {
+            await fetchCategories();
+         }
+
+         if (!transactions || transactions.length === 0) {
+            await fetchTransactions();
+         }
+      };
+
+      if (!initialFetchRef.current) {
+         fetch();
+         initialFetchRef.current = true;
       }
+   }, [
+      user,
+      fetchAuthenticatedUser,
+      categoryTypes,
+      fetchCategoryTypes,
+      categories,
+      fetchCategories,
+      transactions,
+      fetchTransactions,
+   ]);
 
-      if (!categories || categories.length === 0) {
-        await fetchCategories();
-      }
+   const handleBackClick = () => {
+      navigate("/home");
+   };
 
-      if (!transactions || transactions.length === 0) {
-        await fetchTransactions();
-      }
-    };
+   // Modal handlers
+   const handleShowSplitTransactionModal = (transaction) => {
+      setShowSplitTransactionModal(true);
+      setCurrentTransaction(transaction);
+   };
 
-    if (!initialFetchRef.current) {
-      fetch();
-      initialFetchRef.current = true;
-    }
-  }, [
-    user,
-    fetchAuthenticatedUser,
-    categoryTypes,
-    fetchCategoryTypes,
-    categories,
-    fetchCategories,
-    transactions,
-    fetchTransactions,
-  ]);
+   const handleCloseSplitTransactionModal = () => {
+      setShowSplitTransactionModal(false);
+   };
 
-  const handleBackClick = () => {
-    navigate("/home");
-  };
+   const handleShowReduceTransactionModal = (transaction) => {
+      setShowReduceTransactionModal(true);
+      setCurrentTransaction(transaction);
+   };
 
-  // Modal handlers
-  const handleShowSplitTransactionModal = (transaction) => {
-    setShowSplitTransactionModal(true);
-    setCurrentTransaction(transaction);
-  };
+   const handleCloseReduceTransactionModal = () => {
+      setShowReduceTransactionModal(false);
+   };
 
-  const handleCloseSplitTransactionModal = () => {
-    setShowSplitTransactionModal(false);
-  };
+   const handleShowRenameTransactionModal = (transaction) => {
+      setShowRenameTransactionModal(true);
+      setCurrentTransaction(transaction);
+   };
 
-  const handleShowReduceTransactionModal = (transaction) => {
-    setShowReduceTransactionModal(true);
-    setCurrentTransaction(transaction);
-  };
+   const handleCloseRenameTransactionModal = () => {
+      setShowRenameTransactionModal(false);
+   };
 
-  const handleCloseReduceTransactionModal = () => {
-    setShowReduceTransactionModal(false);
-  };
+   const handleShowAssignCategoryModal = (transaction) => {
+      setShowAssignCategoryModal(true);
+      setCurrentTransaction(transaction);
+   };
 
-  const handleShowRenameTransactionModal = (transaction) => {
-    setShowRenameTransactionModal(true);
-    setCurrentTransaction(transaction);
-  };
+   const handleCloseAssignCategoryModal = () => {
+      setShowAssignCategoryModal(false);
+   };
 
-  const handleCloseRenameTransactionModal = () => {
-    setShowRenameTransactionModal(false);
-  };
-
-  const handleShowAssignCategoryModal = (transaction) => {
-    setShowAssignCategoryModal(true);
-    setCurrentTransaction(transaction);
-  };
-
-  const handleCloseAssignCategoryModal = () => {
-    setShowAssignCategoryModal(false);
-  };
-
-  return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-900 to-indigo-800">
-      <FaArrowLeft
-        className="text-3xl text-white ml-5 mt-5 hover:scale-105 hover:text-gray-200 cursor-pointer z-[500]"
-        onClick={handleBackClick}
-      />
-      <div className="flex-1 flex flex-col justify-center items-center px-8 md:px-12">
-        <div className="max-w-md text-center mb-8">
-          <h2 className="text-4xl md:text-5xl font-bold text-white">
-            Explore your{" "}
-            <span className="text-indigo-600"> {categoryType} </span>
-          </h2>
-        </div>
-        <div className="flex flex-wrap justify-center items-center w-full space-y-4">
-          {filteredCategories.length > 0 ? (
-            filteredCategories.map((category) => (
-              <DetailedCategory
-                key={category.categoryId}
-                category={category}
-                handleShowSplitTransactionModal={
-                  handleShowSplitTransactionModal
-                }
-                handleShowReduceTransactionModal={
-                  handleShowReduceTransactionModal
-                }
-                handleShowRenameTransactionModal={
-                  handleShowRenameTransactionModal
-                }
-                handleShowAssignCategoryModal={handleShowAssignCategoryModal}
-              />
-            ))
-          ) : (
-            <p className="text-white">No categories found for this type.</p>
-          )}
-        </div>
+   return (
+      <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-900 to-indigo-800">
+         <FaArrowLeft
+            className="text-3xl text-white ml-5 mt-5 hover:scale-105 hover:text-gray-200 cursor-pointer z-[500]"
+            onClick={handleBackClick}
+         />
+         <div className="flex-1 flex flex-col justify-center items-center px-8 md:px-12">
+            <div className="max-w-md text-center mb-8">
+               <h2 className="text-4xl md:text-5xl font-bold text-white">
+                  Explore your{" "}
+                  <span className="text-indigo-600"> {categoryType} </span>
+               </h2>
+            </div>
+            <div className="flex flex-wrap justify-center items-center w-full space-y-8">
+               {filteredCategories.length > 0 ? (
+                  filteredCategories.map((category) => (
+                     <DetailedCategory
+                        key={category.categoryId}
+                        category={category}
+                        handleShowSplitTransactionModal={
+                           handleShowSplitTransactionModal
+                        }
+                        handleShowReduceTransactionModal={
+                           handleShowReduceTransactionModal
+                        }
+                        handleShowRenameTransactionModal={
+                           handleShowRenameTransactionModal
+                        }
+                        handleShowAssignCategoryModal={
+                           handleShowAssignCategoryModal
+                        }
+                     />
+                  ))
+               ) : (
+                  <p className="text-white">
+                     No categories found for this type.
+                  </p>
+               )}
+            </div>
+         </div>
+         {/* Modals */}
+         {showSplitTransactionModal && (
+            <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
+               <SplitTransactionModal
+                  onClose={handleCloseSplitTransactionModal}
+                  transaction={currentTransaction}
+               />
+            </div>
+         )}
+         {showReduceTransactionModal && (
+            <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
+               <ReduceTransaction
+                  onClose={handleCloseReduceTransactionModal}
+                  transaction={currentTransaction}
+               />
+            </div>
+         )}
+         {showRenameTransactionModal && (
+            <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
+               <RenameTransaction
+                  onClose={handleCloseRenameTransactionModal}
+                  transaction={currentTransaction}
+               />
+            </div>
+         )}
+         {showAssignCategoryModal && (
+            <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
+               <AssignCategoryModal
+                  onClose={handleCloseAssignCategoryModal}
+                  transaction={currentTransaction}
+               />
+            </div>
+         )}
       </div>
-      {/* Modals */}
-      {showSplitTransactionModal && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
-          <SplitTransactionModal
-            onClose={handleCloseSplitTransactionModal}
-            transaction={currentTransaction}
-          />
-        </div>
-      )}
-      {showReduceTransactionModal && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
-          <ReduceTransaction
-            onClose={handleCloseReduceTransactionModal}
-            transaction={currentTransaction}
-          />
-        </div>
-      )}
-      {showRenameTransactionModal && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
-          <RenameTransaction
-            onClose={handleCloseRenameTransactionModal}
-            transaction={currentTransaction}
-          />
-        </div>
-      )}
-      {showAssignCategoryModal && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex justify-center items-center">
-          <AssignCategoryModal
-            onClose={handleCloseAssignCategoryModal}
-            transaction={currentTransaction}
-          />
-        </div>
-      )}
-    </div>
-  );
+   );
 };
 
 export default CategoryTypePage;
