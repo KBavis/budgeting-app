73b0e4f852d6c4bb77adfc0948b26b790dfec72c
Kellen Bavis <kellenbavis10@gmail.com>
2024-07-07 12:16:50 -0400

    fix: updated navbar to only display once users have completed registration

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,61 +1,64 @@
 import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import authContext from "../../context/auth/authContext";
+import transactionContext from "../../context/transaction/transactionContext";
 
 /**
  *  NavBar component to introduce Multipage Navigation and logging out capabilities
  */
 const Navbar = () => {
-   const { user, logout } = useContext(authContext);
-   const navigate = useNavigate();
+  const { user, logout } = useContext(authContext);
+  const { transactions } = useContext(transactionContext);
+  const navigate = useNavigate();
 
-   const handleLogout = () => {
-      logout(); // Call the logout function from the state
-      navigate("/"); // Redirect to the home page
-   };
+  const handleLogout = () => {
+    logout(); // Call the logout function from the state
+    navigate("/"); // Redirect to the home page
+  };
 
-   return (
-      user && (
-         <nav className="absolute top-0 right-0 w-full py-8 z-[100]">
-            <div className="container mx-auto">
-               <ul className="flex justify-end space-x-8 text-sm">
-                  <li>
-                     <Link
-                        to="/transactions"
-                        className="text-white font-bold duration-500 hover:text-indigo-400"
-                     >
-                        Transactions
-                     </Link>
-                  </li>
-                  <li>
-                     <Link
-                        to="/accounts"
-                        className="text-white font-bold duration-500 hover:text-indigo-400"
-                     >
-                        Accounts
-                     </Link>
-                  </li>
-                  <li>
-                     <Link
-                        to="/categories"
-                        className="text-white font-bold duration-500 hover:text-indigo-400"
-                     >
-                        Categories
-                     </Link>
-                  </li>
-                  <li>
-                     <button
-                        onClick={handleLogout}
-                        className="text-white font-bold duration-500 hover:text-indigo-400"
-                     >
-                        Logout
-                     </button>
-                  </li>
-               </ul>
-            </div>
-         </nav>
-      )
-   );
+  return (
+    user &&
+    transactions && (
+      <nav className="absolute top-0 right-0 w-full py-8 z-[100]">
+        <div className="container mx-auto">
+          <ul className="flex justify-end space-x-8 text-sm">
+            <li>
+              <Link
+                to="/transactions"
+                className="text-white font-bold duration-500 hover:text-indigo-400"
+              >
+                Transactions
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/accounts"
+                className="text-white font-bold duration-500 hover:text-indigo-400"
+              >
+                Accounts
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/categories"
+                className="text-white font-bold duration-500 hover:text-indigo-400"
+              >
+                Categories
+              </Link>
+            </li>
+            <li>
+              <button
+                onClick={handleLogout}
+                className="text-white font-bold duration-500 hover:text-indigo-400"
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  );
 };
 
 export default Navbar;
