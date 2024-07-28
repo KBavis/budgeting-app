551ae149010dbe217e2aa940050108d9f7c958c1
Kellen Bavis <kellenbavis10@gmail.com>
2024-07-28 16:53:23 -0400

    feat:created scheduled Job to run at the first of every month to generate BudgetPerformance entities for each active user

---
+++ backend/src/main/java/com/bavis/budgetapp/config/ScheduledTasksConfig.java
@@ -0,0 +1,25 @@
+package com.bavis.budgetapp.config;
+
+import com.bavis.budgetapp.service.BudgetPerformanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Configuration class used to set up scheduled tasks
+ *
+ */
+@Component
+public class ScheduledTasksConfig {
+
+    @Autowired
+    private BudgetPerformanceService budgetPerformanceService;
+
+    /**
+     * Generate User's BudgetPerformances on the first day of every month
+     */
+    @Scheduled(cron = "0 0 0 1 * ?")
+    public void generateBudgetPerformance() {
+        budgetPerformanceService.runGenerateBudgetPerformanceJob();
+    }
+}
