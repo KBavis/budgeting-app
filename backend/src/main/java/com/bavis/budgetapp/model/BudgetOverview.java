52f2029594fa187650c5fafd636a6daf8cca8b2d
Kellen Bavis <kellenbavis10@gmail.com>
2024-07-29 20:33:15 -0400

    refactor: ensure BudgetOverview emeddable and enum is annotated accordingly

--- backend/src/main/java/com/bavis/budgetapp/model/BudgetOverview.java
+++ backend/src/main/java/com/bavis/budgetapp/model/BudgetOverview.java
@@ -1,6 +1,9 @@
 package com.bavis.budgetapp.model;
 
 import com.bavis.budgetapp.constants.OverviewType;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +13,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Embeddable
 public class BudgetOverview {
+    @Enumerated(EnumType.STRING)
     private OverviewType overviewType;
+
     private double totalSpent;
+
     private double totalAmountAllocated;
+
     private double totalPercentUtilized;
 }
