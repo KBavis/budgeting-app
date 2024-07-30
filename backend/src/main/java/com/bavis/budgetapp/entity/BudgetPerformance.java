91777538884e30b3dd21c9e9c017065477a701c7
Kellen Bavis <kellenbavis10@gmail.com>
2024-07-29 20:34:08 -0400

    refactor: added naming conventions to budgetOverview attributes
    added unique naming conventions for budgetOverview attributes to ensure each column in BudgetPerformance is uniquely identifiable

--- backend/src/main/java/com/bavis/budgetapp/entity/BudgetPerformance.java
+++ backend/src/main/java/com/bavis/budgetapp/entity/BudgetPerformance.java
@@ -2,12 +2,12 @@ package com.bavis.budgetapp.entity;
 
 import com.bavis.budgetapp.model.BudgetOverview;
 import com.bavis.budgetapp.model.BudgetPerformanceId;
-import com.bavis.budgetapp.model.MonthYear;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.IdClass;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,21 +19,48 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@IdClass(BudgetPerformanceId.class)
 public class BudgetPerformance {
-    @Id
-    @Embedded
-    private MonthYear monthYear;
-
-    @Id
-    @ManyToOne
-    private User user;
+    @EmbeddedId
+    @Column(name = "BUDGET_PERFORMANCE_ID", nullable = false)
+    private BudgetPerformanceId id;
 
     @OneToMany
+    @Column(name = "CATEGORIES", nullable = false)
     private List<Category> categories;
 
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "overviewType", column = @Column(name = "GENERAL_OVERVIEW_TYPE")),
+            @AttributeOverride(name = "totalSpent", column = @Column(name = "GENERAL_TOTAL_SPENT")),
+            @AttributeOverride(name = "totalAmountAllocated", column = @Column(name = "GENERAL_TOTAL_AMOUNT_ALLOCATED")),
+            @AttributeOverride(name = "totalPercentUtilized", column = @Column(name = "GENERAL_TOTAL_PERCENT_UTILIZED"))
+    })
     private BudgetOverview generalOverview;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "overviewType", column = @Column(name = "NEEDS_OVERVIEW_TYPE")),
+            @AttributeOverride(name = "totalSpent", column = @Column(name = "NEEDS_TOTAL_SPENT")),
+            @AttributeOverride(name = "totalAmountAllocated", column = @Column(name = "NEEDS_TOTAL_AMOUNT_ALLOCATED")),
+            @AttributeOverride(name = "totalPercentUtilized", column = @Column(name = "NEEDS_TOTAL_PERCENT_UTILIZED"))
+    })
     private BudgetOverview needsOverview;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "overviewType", column = @Column(name = "WANTS_OVERVIEW_TYPE")),
+            @AttributeOverride(name = "totalSpent", column = @Column(name = "WANTS_TOTAL_SPENT")),
+            @AttributeOverride(name = "totalAmountAllocated", column = @Column(name = "WANTS_TOTAL_AMOUNT_ALLOCATED")),
+            @AttributeOverride(name = "totalPercentUtilized", column = @Column(name = "WANTS_TOTAL_PERCENT_UTILIZED"))
+    })
     private BudgetOverview wantsOverview;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "overviewType", column = @Column(name = "INVESTMENTS_OVERVIEW_TYPE")),
+            @AttributeOverride(name = "totalSpent", column = @Column(name = "INVESTMENTS_TOTAL_SPENT")),
+            @AttributeOverride(name = "totalAmountAllocated", column = @Column(name = "INVESTMENTS_TOTAL_AMOUNT_ALLOCATED")),
+            @AttributeOverride(name = "totalPercentUtilized", column = @Column(name = "INVESTMENTS_TOTAL_PERCENT_UTILIZED"))
+    })
     private BudgetOverview investmentOverview;
 }
