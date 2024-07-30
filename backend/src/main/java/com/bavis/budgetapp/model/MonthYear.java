e4978c67f253147bcacbc3cfd55fb49f6c303cb5
Kellen Bavis <kellenbavis10@gmail.com>
2024-07-29 20:34:30 -0400

    chore: added EqualsAndHashCode annotation

--- backend/src/main/java/com/bavis/budgetapp/model/MonthYear.java
+++ backend/src/main/java/com/bavis/budgetapp/model/MonthYear.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Builder
 @Embeddable
+@EqualsAndHashCode
 public class MonthYear implements Serializable {
     private String month;
     private int year;
