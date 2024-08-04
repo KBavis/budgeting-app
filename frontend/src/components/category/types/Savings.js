e8399423d6a0aabaeadd6aec15b47b581068fe87
Kellen Bavis <kellenbavis10@gmail.com>
2024-08-03 20:23:51 -0400

    refactor: update styling/wording of Savings
    updated wording and styling of the Savings
    component to better convey its meaning

--- frontend/src/components/category/types/Savings.js
+++ frontend/src/components/category/types/Savings.js
@@ -2,8 +2,8 @@ import React, { useContext } from "react";
 import categoryTypeContext from "../../../context/category/types/categoryTypeContext";
 
 /**
- * Component that will display the difference in users allocated budget and their monthly income
- * @returns
+ * Component that displays the expected savings for the current month based on budget allocations.
+ * @returns {JSX.Element} The Savings component.
  */
 const Savings = () => {
   const { categoryTypes } = useContext(categoryTypeContext);
@@ -14,17 +14,17 @@ const Savings = () => {
   }, 0);
 
   return (
-    <div className="bg-gray-800 text-white p-2 rounded-md shadow-sm w-1/3 md:w-1/4 lg:w-1/5 text-center mb-4 border-[2px] border-indigo-500">
-      <div className="flex flex-col items-center">
-        <h2 className="text-lg font-bold mb-1">
-          Savings Per Month:{"    "}
-          <span className="ml-4 text-2xl font-semibold text-green-500">
-            ${totalSavedAmount.toFixed(2)}
-          </span>
-        </h2>
+      <div className="bg-gray-800 text-white p-4 rounded-md shadow-md w-full md:w-2/3 lg:w-1/4 text-center mb-6 border-2 border-indigo-500">
+        <div className="flex flex-col items-center">
+          <h2 className="text-lg font-bold mb-2">
+            Your Expected Savings for This Month:
+          </h2>
+          <span className="text-2xl font-semibold text-green-500">
+          ${totalSavedAmount.toFixed(2)}
+        </span>
+        </div>
       </div>
-    </div>
   );
 };
 
-export default Savings;
+export default Savings;;
