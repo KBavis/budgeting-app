34c0a05f9be3657742fb41a13fe05e9ea4bcb5f2
Kellen Bavis <kellenbavis10@gmail.com>
2024-08-11 00:36:48 -0400

    feat: added trash icon to signify deletion of account

--- frontend/src/components/accounts/Account.js
+++ frontend/src/components/accounts/Account.js
@@ -1,20 +1,34 @@
-/**
- *  Account component to represent a users account
- */
 import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 /**
- *  Account component to represent a user's account
+ * Component to represent a users Account
+ *
+ * @param account
+ *          - account to generate component for
+ * @param handleShowConfirmationModal
+ *          - functionality to display confirmation modal
  */
-const Account = ({ account }) => {
+const Account = ({ account, handleShowConfirmationModal }) => {
     return (
-        <div className="bg-gradient-to-r from-indigo-800 to-indigo-400 rounded-xl shadow-lg p-8 mb-8 w-full max-w-3xl text-center transform transition duration-300 hover:scale-105">
-            <div className="flex justify-center items-center mb-4">
-                <h3 className="text-4xl font-bold text-white">{account.accountName} </h3>
+        <div className="relative bg-gradient-to-r from-indigo-800 to-indigo-400 rounded-xl shadow-lg p-8 mb-8 w-full max-w-3xl text-center">
+            <div
+                className="absolute top-4 right-4 text-white cursor-pointer hover:text-red-500 transition duration-200"
+                onClick={() => handleShowConfirmationModal(account)}
+                title="Delete Account"
+            >
+                <span className="z-5 duration-500 hover:text-red-500 "><FaTrashAlt size={24} /> </span>
             </div>
-            <p className="text-2xl text-white mb-4">Balance: <span className="font-semibold">${account.balance.toFixed(2)}</span></p>
+
+            <div className="flex justify-center items-center mb-4">
+                <h3 className="text-4xl font-bold text-white">{account.accountName}</h3>
+            </div>
+            <p className="text-2xl text-white mb-4">
+                Balance: <span className="font-semibold">${account.balance.toFixed(2)}</span>
+            </p>
         </div>
     );
 };
 
 export default Account;
+
