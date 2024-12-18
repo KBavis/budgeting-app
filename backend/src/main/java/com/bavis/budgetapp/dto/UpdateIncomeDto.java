7d795222c647739995504db7e6f2139db1cf476a
Kellen Bavis <kellenbavis10@gmail.com>
2024-12-18 17:36:11 -0500

    feat: added new dto for updating a users income

---
+++ backend/src/main/java/com/bavis/budgetapp/dto/UpdateIncomeDto.java
@@ -0,0 +1,15 @@
+package com.bavis.budgetapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class UpdateIncomeDto {
+    private double amount;
+    private Long incomeId;
+}
