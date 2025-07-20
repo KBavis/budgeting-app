143f7d75194001fcd3419abdea4d33df695e58c0
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-20 11:54:56 -0400

    test: unit tests for verifying updating of account balance

--- backend/src/main/java/com/bavis/budgetapp/dto/PlaidAccountDto.java
+++ backend/src/main/java/com/bavis/budgetapp/dto/PlaidAccountDto.java
@@ -2,6 +2,7 @@ package com.bavis.budgetapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class PlaidAccountDto {
 
     @Data
