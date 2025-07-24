59aa9074bda38f252b8ff8f58f918fdf0b7396f5
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-24 00:12:42 -0400

    feat: add ability to extract passed in user ids access tokens for non-end dated accounts

--- backend/scripts/backfill.py
+++ backend/scripts/backfill.py
@@ -1,15 +1,52 @@
 import argparse
 from dotenv import load_dotenv
 from psycopg2 import connect
-import logging
 import os
 
 def backfill():
 
     args = parse_args()
+    user_ids = args.users
+
     connection = connect_db()
+    access_tokens = retrieve_user_access_tokens(user_ids, connection)
 
 
+
+def retrieve_user_access_tokens(user_ids, connection):
+    placeholders = ','.join(['%s'] * len(user_ids))
+
+    query = f"""
+        SELECT access_token, user_id 
+        FROM connection c 
+        JOIN account a ON a.connection_id = c.connection_id 
+        WHERE is_deleted = false AND user_id IN ({placeholders})
+    """ 
+    
+    access_tokens = {}
+
+    try:
+
+        with connection.cursor() as cur:
+            cur.execute(query, user_ids)
+            rows = cur.fetchall()
+
+            # mapping of user IDs to access tokens 
+            for row in rows:
+                user_id = row[1]
+                token = row[0]
+
+                if user_id not in access_tokens:
+                    access_tokens[user_id] = []
+
+                access_tokens[user_id].append(token)
+            
+            return access_tokens
+
+
+    except Exception as e:
+        print(f"An error occurred while retrieving user's access tokens: {e}")
+        raise e
 
 
 def connect_db():
@@ -47,7 +84,12 @@ def parse_args():
     parser.add_argument(
         "--users",
         nargs='+',
+        required=True,
         help='List of User IDs to backfill transaction meta data for'
     )
     
     return parser.parse_args()
+
+
+if __name__ == "__main__":
+    backfill()
