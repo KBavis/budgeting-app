d3966e3f9a940bf64c39efba1aa1c79a27ca83cc
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-29 01:00:54 -0400

    feat: stub out suggestion endpoint to later be utilized to invoke make category suggestion based on transaction meta data

--- backend/suggestion_engine/service.py
+++ backend/suggestion_engine/service.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
 
 app = FastAPI()
 
+class TransactionMetadata(BaseModel):
+    merchant_name: str
+    amount: float 
+    date_time: datetime
+    detailed: str 
+    primary: str
 
-@app.get("/")
-def read_root():
-    return {"message": "testing!"}
+
+@app.post("/suggestion")
+def category_suggestion(metadata: TransactionMetadata):
+    return {
+        "merchant_name": metadata.merchant_name,
+        "amount": metadata.amount, 
+        "date_time": metadata.date_time, 
+        "plaid_detailed_category": metadata.detailed,
+        "plaid_primary_category": metadata.primary 
+    }
