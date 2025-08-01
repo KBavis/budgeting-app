ace1aebcb7b472b8a570733aee91cb86f29288c0
Kellen Bavis <kellenrbavis@gmail.com>
2025-08-01 01:02:26 -0400

    feat: create mapping from encoded values to original category ID

--- backend/suggestion_engine/predict.py
+++ backend/suggestion_engine/predict.py
@@ -20,13 +20,26 @@ def predict_category(user_id, transaction, nn):
     # convert to tensor 
     tensor = torch.from_numpy(Xs).float()
 
+    # make prediction
     nn.eval()
     with torch.no_grad():
         pred = nn(tensor)
 
+    # retrieve label encoder & create de-coding mapper
+    label_encoder = joblib.load(f"suggestion_engine/artifacts/{user_id}/label_encoder.joblib")
+    original_labels = list(label_encoder.classes_)
+
+    mapper = {}
+    for i in range(len(original_labels)):
+        curr_label = original_labels[i]
+        mapper[i] = curr_label
+    
+
     predicted_classes = torch.argmax(pred, dim=1)
+    encoded_category_id = predicted_classes.item()
     print(predicted_classes)
 
+    print(f"The predicted category ID is {mapper[encoded_category_id]}")
     
 
     # TODO: Determine confidence level and return UncategorizedSuggestion if confidence is too low after making prediction via neural network
