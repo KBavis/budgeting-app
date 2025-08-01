2a12bfd3f4357821e5c50ad794927ca1324ef7d8
Kellen Bavis <kellenrbavis@gmail.com>
2025-08-01 01:10:12 -0400

    feat: return response

--- backend/suggestion_engine/predict.py
+++ backend/suggestion_engine/predict.py
@@ -5,19 +5,19 @@ from suggestion_engine import data
 import joblib
 import pandas as pd
 
+
 def predict_category(user_id, transaction, nn):
     print(f"Attempting to predict the Category for the following Transaction metadata: {transaction}")
+
+    # load joblibs from training
+    preprocesor = joblib.load(f'suggestion_engine/artifacts/{user_id}/preprocessor.joblib')
+    label_encoder = joblib.load(f"suggestion_engine/artifacts/{user_id}/label_encoder.joblib")
 
     # prepare input data 
     txs = [transaction.dict()]
     features, _ = data.prepare_input(txs)
     df = pd.DataFrame(features, columns=['amount', 'hour', 'day', 'merchant', 'primary_category', 'detailed_category'])
-
-    # load preprocessor & invoke
-    preprocesor = joblib.load(f'suggestion_engine/artifacts/{user_id}/preprocessor.joblib')
     Xs = preprocesor.transform(df)
-
-    # convert to tensor 
     tensor = torch.from_numpy(Xs).float()
 
     # make prediction
@@ -25,21 +25,18 @@ def predict_category(user_id, transaction, nn):
     with torch.no_grad():
         pred = nn(tensor)
 
-    # retrieve label encoder & create de-coding mapper
-    label_encoder = joblib.load(f"suggestion_engine/artifacts/{user_id}/label_encoder.joblib")
+    # create encoded value mapping
     original_labels = list(label_encoder.classes_)
-
-    mapper = {}
+    encoded_map = {}
     for i in range(len(original_labels)):
         curr_label = original_labels[i]
-        mapper[i] = curr_label
+        encoded_map[i] = curr_label
     
 
     predicted_classes = torch.argmax(pred, dim=1)
-    encoded_category_id = predicted_classes.item()
-    print(predicted_classes)
-
-    print(f"The predicted category ID is {mapper[encoded_category_id]}")
+    category_id = encoded_map[predicted_classes.item()]
     
 
     # TODO: Determine confidence level and return UncategorizedSuggestion if confidence is too low after making prediction via neural network
+    return CategorySuggestion(category_id=category_id, confidence=65.50, source="PersonalModel")
+    
