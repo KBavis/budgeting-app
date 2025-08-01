1fefffd653322f138620c59d2627de34b97acbe3
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-31 23:50:13 -0400

    fix: update relative dir to be correct

--- backend/suggestion_engine/service.py
+++ backend/suggestion_engine/service.py
@@ -27,7 +27,7 @@ class CategorySuggestionRequest(BaseModel):
 def category_suggestion(request: CategorySuggestionRequest):
 
     user_id = request.user_id
-    file_path_str = f"../artifacts/{user_id}/model_weights.pth"
+    file_path_str = f"artifacts/{user_id}/model_weights.pth"
     path = Path(file_path_str)
 
     if path.is_file():
