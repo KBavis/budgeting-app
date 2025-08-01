69030f118d79ad24a3751c87a8ecf5dafab08d43
Kellen Bavis <kellenrbavis@gmail.com>
2025-07-31 23:58:29 -0400

    feat: retrieve metadata assocaited with persisted model

--- backend/suggestion_engine/service.py
+++ backend/suggestion_engine/service.py
@@ -6,6 +6,7 @@ import torch
 from suggestion_engine.models.classifer import CategoryPredictor
 from suggestion_engine.models.context_mapper import ContextMapper
 from suggestion_engine import predict
+import json
 
 
 app = FastAPI()
@@ -28,11 +29,17 @@ def category_suggestion(request: CategorySuggestionRequest):
 
     user_id = request.user_id
     file_path_str = f"artifacts/{user_id}/model_weights.pth"
+    meta_data_path_str = f"artifacts/{user_id}/metadata.json"
     path = Path(file_path_str)
 
     if path.is_file():
         print(f"User {user_id} has a corresponding Personal model; utilizing model for prediction")
-        nn = CategoryPredictor()
+
+        # load meta data 
+        with open(meta_data_path_str, "r") as metadata:
+            data = json.load(metadata)
+
+        nn = CategoryPredictor(data['input_dim'], data['num_classes'])
         nn.load_state_dict(torch.load(file_path_str, weights_only=True))
         suggestion = predict.predict_category(request.transaction)
     else:
