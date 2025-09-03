f2dca6fee12534e581e8a2b7dc87daa350abebd8
Kellen Bavis <kellenrbavis@gmail.com>
2025-09-02 21:43:22 -0400

    fix: update import to be speicfy inference package

--- backend/suggestion_engine/inference/predict.py
+++ backend/suggestion_engine/inference/predict.py
@@ -1,5 +1,5 @@
-from suggestion_engine.outcomes.category_suggestion import CategorySuggestion
-from suggestion_engine.outcomes.uncategorized_suggestion import UncategorizedSuggestion
+from suggestion_engine.inference.outcomes.category_suggestion import CategorySuggestion
+from suggestion_engine.inference.outcomes.uncategorized_suggestion import UncategorizedSuggestion
 from suggestion_engine import data
 import joblib
 import pandas as pd
