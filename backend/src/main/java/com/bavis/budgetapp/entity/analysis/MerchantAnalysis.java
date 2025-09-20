b7742835303abbd6db4568570ada9c5cae747ee2
Kellen Bavis <kellenrbavis@gmail.com>
2025-09-20 19:13:11 -0400

    refactor: use builder annotation and use Double instead of Long

--- backend/src/main/java/com/bavis/budgetapp/entity/analysis/MerchantAnalysis.java
+++ backend/src/main/java/com/bavis/budgetapp/entity/analysis/MerchantAnalysis.java
@@ -1,16 +1,18 @@
 package com.bavis.budgetapp.entity.analysis;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class MerchantAnalysis {
     private String merchantName;
     private Integer merchantRank;
-    private Long totalSpent;
+    private Double totalSpent;
     private Integer transactionCount;
     private Double avgTransactionAmount;
 }
