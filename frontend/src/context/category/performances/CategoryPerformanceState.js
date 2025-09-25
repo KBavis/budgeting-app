977142c37fbebb609a6a8e0af0ac8d5ca1b23635
Kellen Bavis <kellenrbavis@gmail.com>
2025-09-24 21:46:39 -0400

    feat: init necessary context API setup for new necessary data

---
+++ frontend/src/context/category/performances/CategoryPerformanceState.js
@@ -0,0 +1,73 @@
+import { useReducer } from "react";
+import { FETCH_CATEGORY_PERFORMANCES_SUCCESS, FETCH_CATEGORY_PERFORMANCES_ERROR, SET_LOADING, CLEAR_ERRORS } from "./types";
+import categoryPerformanceReducer from "./categoryPerformanceReducer";
+import { initalState } from "../../summary/initialState";
+import apiUrl from "../../../utils/url";
+import axios from "axios";
+
+/**
+ * Global state for Category Performances
+ * 
+ * @param  props 
+ *      - props from App.js
+ */
+const CategoryPerformanceState = (props) => {
+
+    const [state, dispatch] = useReducer(categoryPerformanceReducer, initialState)
+
+    const fetchCategoryPerformances = async (categoryTypeId, monthYear) => {
+        if (localStorage.token) {
+            setAuthToken(localStorage.token);
+        }
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        try {
+            const res = await axios.get(
+                `${apiUrl}/category/performance/${categoryTypeId}`,
+                monthYear,
+                config
+            );
+            dispatch({
+                type: FETCH_CATEGORY_PERFORMANCES_SUCCESS,
+                payload: res.data
+            })
+        } catch (err) {
+            console.error(err);
+            dispatch({
+                type: FETCH_CATEGORY_PERFORMANCES_ERROR,
+                payload: err.response.data.error
+            })
+        }
+    }
+
+    /**
+     * Clear any errors out of context 
+     */
+    const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+
+    /**
+     *  Functionality to set loading to true
+     */
+    const setLoading = () => dispatch({ type: SET_LOADING });
+
+
+    return (
+        <CategoryPerformanceState.Provider value={{
+            category_performances: state.category_performances,
+            loading: state.loading,
+            error: state.error,
+            fetchCategoryPerformances,
+            setLoading,
+            clearErrors
+        }}>
+            {props.children}
+        </CategoryPerformanceState.Provider>
+    )
+}
+
+export default CategoryPerformanceState;
