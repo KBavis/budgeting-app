6d00910b75ee8a382eb2abd22e142ed0c820f04c
Kellen Bavis <kellenrbavis@gmail.com>
2025-09-28 19:39:33 -0400

    feat: fix logic for determining which catgory type the current session of spending analysis page corresponds to

--- frontend/src/pages/SpendingAnalysisPage.js
+++ frontend/src/pages/SpendingAnalysisPage.js
@@ -1,28 +1,55 @@
 import { useNavigate, useParams } from "react-router-dom"
 import categoryTypeContext from "../context/category/types/categoryTypeContext"
+import CategoryPerformanceContext from "../context/category/performances/categoryPerformanceContext"
 import { useContext, useEffect, useState } from "react"
 
 const SpendingAnalysisPage = () => {
 
     const { categoryTypes } = useContext(categoryTypeContext)
+    const { fetchCategoryPerformances } = useContext(CategoryPerformanceContext)
 
     const [currentType, setCurrentType] = useState(null)
     const navigate = useNavigate()
     const { type, month, year } = useParams()
 
+    // determine the relevant Category Type the user selected 
     useEffect(() => {
+        if (categoryTypes && categoryTypes.length >= 3) {
 
-        // find selected category type 
-        if (categoryTypes) {
-            const curr = categoryTypes?.filter(cat => cat.name === type)
+            // set currentType equal to CategoryType in state based on name in URL 
+            for (let i = 0; i < categoryTypes.length; i++) {
+                if (categoryTypes[i].name.toLowerCase() == type) {
+                    setCurrentType(categoryTypes[i])
+                }
+            }
+
         }
 
     }, [categoryTypes])
 
+    // retrieve relevant CategoryPerformances corresponding to month, year, and CategoryType
+    useEffect(() => {
+
+
+        const fetch = async () => {
+            let monthYear = { "month": month.toUpperCase(), "year": parseInt(year) }
+            await fetchCategoryPerformances(parseInt(currentType.categoryTypeId), monthYear)
+        }
+
+        if (currentType) {
+            fetch()
+        }
+
+    }, [currentType])
+
+
     return (
         // gradient background on page
         <div className="flex flex-col min-h-screen bg-gradient-to-br from-gray-900 to-indigo-800 relative">
-            {/* back button for navigation back to Budget Summary page  */}
+            {/* back button for navigation back to Budget Summary page 
+                    TODO:  Create state variable to track previously selected month year so when we navigate back
+                    to home page, we can "remember" which budget summary we have open (like a prevSummary initially set to null)
+            */}
             <button
                 onClick={() => navigate(-1)}
                 className="fixed top-4 left-4 z-50 bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-3 rounded shadow-lg hover:cursor-pointer"
