0410a923dd8fee49d26ef44363d6532c0c13502b
Kellen Bavis <kellenrbavis@gmail.com>
2025-09-28 13:10:02 -0400

    chore: use sl4j annotation and remove tests for controller

--- backend/src/main/java/com/bavis/budgetapp/controller/CategoryPerformanceController.java
+++ backend/src/main/java/com/bavis/budgetapp/controller/CategoryPerformanceController.java
@@ -4,17 +4,21 @@ import com.bavis.budgetapp.entity.analysis.MonthlyCategoryPerformance;
 import com.bavis.budgetapp.model.MonthYear;
 import com.bavis.budgetapp.service.MonthlyCategoryPerformanceService;
 import jakarta.validation.Valid;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@Log4j2
+@Slf4j
 @RequestMapping("/category/performance")
 public class CategoryPerformanceController {
 
+    private static final Logger log = LoggerFactory.getLogger(CategoryPerformanceController.class);
     @Autowired
     private MonthlyCategoryPerformanceService categoryPerformanceService;
 
